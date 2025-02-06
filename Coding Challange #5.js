7cae78c24a930cd0eaaea453780e0fc7f433b7c0
MbiluM <mabilu@usf.edu>
2025-02-05 20:24:48 -0500

    Task 1 - Customer Profile

---
+++ Coding Challange #5.js
@@ -0,0 +1,10 @@
+// Task 1 - Customer Profile
+let customer = { //Customer Infomation
+    name: "Johnj Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+console.log('name: ${customer.name}') //Log each property
+console.log('age: ${customer.age}')
+console.log('emial: ${customer.email}')
+
