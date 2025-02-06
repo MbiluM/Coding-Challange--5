caa5ea5a9e25a6ace048d6906b8545c3cf592d83
MbiluM <mabilu@usf.edu>
2025-02-05 20:34:58 -0500

    Task 2 - Order Details

--- Coding Challange #5.js
+++ Coding Challange #5.js
@@ -8,3 +8,14 @@ console.log('name: ${customer.name}') //Log each property
 console.log('age: ${customer.age}')
 console.log('emial: ${customer.email}')
 
+// Task 2 - Order Details
+const order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function () {
+        console.log ('OrderID: ${this.orderID}');
+        console.log ('total Amount: ${this.totalAmount}');
+        console.log ('Status: ${this.status}');
+    }
+};
